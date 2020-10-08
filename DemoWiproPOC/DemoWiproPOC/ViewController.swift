a2fb5b7192d366c558070fce6576ad7d86a0786c
Piyush <piyush.dwivedi@infogain.com>
2020-10-08 13:18:22 +0530

    initiated xcode project

---
+++ DemoWiproPOC/DemoWiproPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoWiproPOC
+//
+//  Created by Piyush on 08/10/20.
+//  Copyright © 2020 Piyush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
