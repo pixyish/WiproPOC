c7ec59ebdab14bf9a512fb2b31bf7752b2dea9f0
Piyush <piyush.dwivedi@infogain.com>
2020-10-08 13:35:08 +0530

    removed initial unnecessary files and set view controller programmatically

--- DemoWiproPOC/DemoWiproPOC/ViewController/WPListViewController.swift
+++ DemoWiproPOC/DemoWiproPOC/ViewController/WPListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WPListViewController.swift
 //  DemoWiproPOC
 //
 //  Created by Piyush on 08/10/20.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WPListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
+    
 }
-
