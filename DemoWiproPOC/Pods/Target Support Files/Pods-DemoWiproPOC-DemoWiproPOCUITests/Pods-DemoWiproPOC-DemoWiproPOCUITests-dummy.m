00c5bd612c76fbb3bd1621c08e6a31f894248e11
Piyush <piyush.dwivedi@infogain.com>
2020-10-08 16:20:33 +0530

    pod integrated

---
+++ DemoWiproPOC/Pods/Target Support Files/Pods-DemoWiproPOC-DemoWiproPOCUITests/Pods-DemoWiproPOC-DemoWiproPOCUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoWiproPOC_DemoWiproPOCUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoWiproPOC_DemoWiproPOCUITests
+@end
---
+++ DemoWiproPOC/Pods/Target Support Files/Pods-DemoWiproPOC/Pods-DemoWiproPOC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoWiproPOC : NSObject
+@end
+@implementation PodsDummy_Pods_DemoWiproPOC
+@end
