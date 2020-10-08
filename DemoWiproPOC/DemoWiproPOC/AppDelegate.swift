24c5f4c3465a20937c624c31f1a4f382077e6590
Piyush <piyush.dwivedi@infogain.com>
2020-10-08 18:23:55 +0530

    final touch minor change version set for ios 11

--- DemoWiproPOC/DemoWiproPOC/AppDelegate.swift
+++ DemoWiproPOC/DemoWiproPOC/AppDelegate.swift
@@ -11,21 +11,38 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var navigationController: UINavigationController?
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let version = UIDevice.current.systemVersion
+        print(Int(Float(version) ?? 80))
+        if Int(Float(version) ?? 0) < 13 {
+            
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.backgroundColor = UIColor.white
+            
+            // set view controller
+            let vc = WPListViewController()
+            navigationController = UINavigationController(rootViewController: vc)
+            navigationController?.navigationBar.isTranslucent = false
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
